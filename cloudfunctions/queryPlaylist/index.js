aca10872d295b69a4c6ae865e2e8c313b271fe5d
insthink <insthink@192.168.0.128>
2020-02-27 19:12:56 +0800

    playlist api

--- cloudfunctions/queryPlaylist/index.js
+++ cloudfunctions/queryPlaylist/index.js
@@ -10,6 +10,8 @@ const URL = 'http://musicapi.xiecheng.live/personalized'
 const db = cloud.database()
 const playlistTable = db.collection('playlist')
 
+const MAX_LIMIT = 100
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   // 获取播放列表
@@ -17,12 +19,40 @@ exports.main = async (event, context) => {
     return JSON.parse(res).result
   })
 
+  // 分批请求数据库
+  const dbCount = await playlistTable.count()
+  const total = dbCount.total
+  const batchTimes = Math.ceil(total / MAX_LIMIT)
+  let dataPList = [];
+  for (let i = 0; i < batchTimes; ++i) {
+    const dataP = playlistTable
+                        .skip(MAX_LIMIT * i)
+                        .limit(MAX_LIMIT)
+                        .get()
+    dataPList.push(dataP)
+  }
+
+  let dataObject = {
+    data: []
+  }
+  if (dataPList.length > 0) {
+    await Promise.all(dataPList).then(
+      resList => {
+        dataObject = resList.reduce((acc, cur) => {
+          return {
+            data: acc.data.concat(cur.data)
+          }
+        })
+      }
+    )
+  }
+  
   // 去重(直接在.get()后面加.data是不行的)
-  const dbGet = await playlistTable.get()
-  const addList = removeDuplicates(playlist, dbGet.data)
+  // const dbGet = await playlistTable.get()
+  const addList = removeDuplicates(playlist, dataObject.data)
 
   // 插入数据库
-  for (let i = 0; i < addList.length; i++) {
+  for (let i = 0; i < addList.length; ++i) {
     await playlistTable.add({
       data: {
         ...addList[i],
@@ -32,6 +62,9 @@ exports.main = async (event, context) => {
     .then(() => console.log('insert success'))
     .catch(err => console.log(err))
   }
+
+  // 返回新增数据的长度
+  return addList.length
   
 }
 
@@ -41,3 +74,4 @@ removeDuplicates = (rqList, dbList) => {
   // 在请求到的list中找到现有dblist中没有的
   return rqList.filter(item => dbIdList.indexOf(item.id) < 0)
 }
+
